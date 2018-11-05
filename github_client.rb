cdd820a9ef17309d07ab7d51acb56867818f5c60
dornby <12762571+dornby@users.noreply.github.com>
2018-11-05 21:15:15 +0100

    Change sorting method

--- github_client.rb
+++ github_client.rb
@@ -1,14 +1,10 @@
 require "faraday"
 require "json"
 
-response = Faraday.get 'https://api.github.com/repos/victormours/gitehub/issues'
+response = Faraday.get 'https://api.github.com/repos/victormours/gitehub/issues', {:direction => 'asc'}
 
 issues = JSON.parse(response.body)
 
-sorted_issues = issues.sort_by do |issue|
-  issue["number"]
-end
-
-sorted_issues.each do |issue|
+issues.each do |issue|
   p issue["title"]
 end
